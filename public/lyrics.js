7daeb0063ce6c6e751d500deac59f4aa52eb2336
Jake Wilson <jakewilsonfl@gmail.com>
2017-04-22 00:04:12 -0400

    Adds barebones html layer.

---
+++ public/lyrics.js
@@ -0,0 +1,23 @@
+function getLyrics(artist) {
+    document.getElementById('results').innerHTML = 'Loading...';
+    fetch('/artist')
+    .then((blob) => {
+        return blob.json();
+    })
+    .then((res) => {
+        document.getElementById('results').innerHTML = 'done!';
+        console.log(res);
+    })
+    ;
+}
+
+let artistInput = document.getElementById('artist');
+document.getElementById('get_lyrics').addEventListener('click', (e) => {
+    getLyrics(artistInput.value);
+});
+
+artistInput.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) { // enter key
+        getLyrics(artistInput.value);
+    }
+});
