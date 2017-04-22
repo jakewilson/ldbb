ae70f090e6ec5d8b9dd2a08dc2f537212255d86c
Jake Wilson <jakewilsonfl@gmail.com>
2017-04-22 11:52:19 -0400

    Moves access token reading into lyrics.js. Adds callGenius().

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var app = express();
 
-var fs = require('fs');
-
 var lyrics = require('./lyrics.js');
-
-var accessToken = '';
-
-fs.readFile('token.access', 'utf8', (err, data) => {
-    if (err)
-        console.log('could not read access token');
-
-    accessToken = data.trim();
-});
 
 // serve files in public/
 app.use(express.static('public'));
@@ -20,7 +9,7 @@ app.use(express.static('public'));
 app.get('/artist/*', (req, res) => {
     let artist = req.params[0];
 
-    lyrics.get(artist, accessToken, (data) => {
+    lyrics.get(artist, (data) => {
         res.send(data);
     });
 });
--- lyrics.js
+++ lyrics.js
@@ -1,36 +1,44 @@
 var https = require('https');
 
+var ACCESS_TOKEN = '';
+
 module.exports = {
     /**
      * Calls `callback` with all lyrics for an artist
      *
      * @param artist: the artist to search for
-     * @param token: the genius API access token
      * @param callback: function to call once the lyrics have been retreived
      */
-    get: function(artist, token, callback) {
-        getArtistId(artist, token, (id) => {
-            console.log(`${artist} id: ${id}`);
+    get: function(artist, callback) {
+        getArtistId(artist, (id) => {
+            console.log(`ID: ${id}`);
+//            getSongURLS(id, (urls) => {
+//                console.log(`URLS: ${urls}`);
+//            });
         });
     }
 };
 
 /**
+ * Given an artist id, calls `callback` with an array of all song URLS for that artist
+ *
+ * @param id: the artist id to search for
+ * @param callback: function to call once the song URLS have been retrieved
+ */
+function getSongURLs(id, callback) {
+    console.log(`id: ${id}`);
+}
+
+/**
  * Given an artist, calls `callback` with the genius ID for that artist
  *
  * @param artist: the artist to search for
- * @param token: the genius API access token
  * @param callback: function to call once the artist ID has been retrieved
  */
-function getArtistId(artist, token, callback) {
+function getArtistId(artist, callback) {
     artist = artist.toLowerCase().trim();
-    var options = {
-        host: 'api.genius.com',
-        path: '/search?q=' + artist + '&access_token=' + token,
-        method: 'GET'
-    };
     var resStr = '';
-    https.request(options, (res) => {
+    callGenius('/search?q=' + artist, (res) => {
         res.on('data', (data) => {
             resStr += data;
         });
@@ -54,5 +62,32 @@ function getArtistId(artist, token, callback) {
             if (!found) // TODO get more songs?
                 return callback(-1);
         });
-    }).end();
+    });
 }
+
+/**
+ * Performs an HTTPS request on the Genius API and calls callback
+ *
+ * @param path: the path to call the API on
+ * @param callback: the function to call on response
+ */
+function callGenius(path, callback) {
+    var options = {
+        host: 'api.genius.com',
+        path: path,
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + ACCESS_TOKEN
+        }
+    };
+    https.request(options, callback).end();
+}
+
+/* get the access token before doing anything */
+require('fs').readFile('token.access', 'utf8', (err, data) => {
+    if (err)
+        console.log('could not read access token');
+
+    ACCESS_TOKEN = data.trim();
+});
+
