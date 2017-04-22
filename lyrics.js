3aa238ebaf062c2986bb637c771a21c456749d00
Jake Wilson <jakewilsonfl@gmail.com>
2017-04-22 11:28:27 -0400

    Adds getArtistId().

--- lyrics.js
+++ lyrics.js
@@ -2,21 +2,46 @@ var https = require('https');
 
 module.exports = {
     get: function(artist, token, callback) {
-        artist = artist.trim();
-        var options = {
-            host: 'api.genius.com',
-            path: '/search?q=' + artist + '&access_token=' + token,
-            method: 'GET'
-        };
-        var resStr = '';
-        https.request(options, (res) => {
-            console.log(`STATUS: ${res.statusCode}`);
-            res.on('data', (data) => {
-                resStr += data;
-            });
-            res.on('end', () => {
-                callback(resStr.toString());
-            });
-        }).end();
+        getArtistId(artist, token, (id) => {
+            console.log(`${artist} id: ${id}`);
+        });
     }
 };
+
+/**
+ * Given an artist, calls `callback` with the genius ID for that artist
+ */
+function getArtistId(artist, token, callback) {
+    artist = artist.toLowerCase().trim();
+    var options = {
+        host: 'api.genius.com',
+        path: '/search?q=' + artist + '&access_token=' + token,
+        method: 'GET'
+    };
+    var resStr = '';
+    https.request(options, (res) => {
+        res.on('data', (data) => {
+            resStr += data;
+        });
+        res.on('end', () => {
+            var resJson = JSON.parse(resStr);
+            console.log(`STATUS: ${resJson.meta.status}`);
+            if (resJson.meta.status !== 200)
+                return callback(-1);
+
+            var hits = resJson.response.hits;
+            var found = false;
+
+            hits.forEach((hit) => {
+                var resArtist = hit['result']['primary_artist'];
+                if (resArtist['name'].toLowerCase() === artist) {
+                    found = true;
+                    return callback(resArtist['id']);
+                }
+            });
+
+            if (!found)
+                return callback(-1);
+        });
+    }).end();
+}
